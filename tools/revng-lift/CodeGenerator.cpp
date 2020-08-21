77b53a52c631750db1ff7f8b19bab9ba566823c7
sonnyT <tianlinan@iie.ac.cn>
2020-08-21 22:55:19 +0800

    Change statistics of ret 3

--- tools/revng-lift/CodeGenerator.cpp
+++ tools/revng-lift/CodeGenerator.cpp
@@ -991,7 +991,7 @@ void CodeGenerator::translate(uint64_t VirtualAddress) {
       Builder.CreateUnreachable();
     }
     if(*ptc.isRet)
-      JumpTargets.harvestRetBlocks(NextPC,NextPC);
+      JumpTargets.harvestRetBlocks(*ptc.isRet,NextPC);
     if(*ptc.isDirectJmp or *ptc.isIndirectJmp or *ptc.isCall)
       JumpTargets.harvestNextAddrofBr(NextPC);
       
